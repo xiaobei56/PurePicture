d281c69137b462d4ddc99d96c52aafef8f0c3b4a
xiaobei56 <xiaobei_63@163.com>
2019-11-20 19:21:20 +0800

    fdfd

--- app/src/main/java/cn/gridlife/purepicture/MainActivity.java
+++ app/src/main/java/cn/gridlife/purepicture/MainActivity.java
@@ -2,13 +2,34 @@ package cn.gridlife.purepicture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSelectPicture;
+    Button btnSelectAlbum;
+    final int PICTURES_REQUEST_CODE = 0x0001;
+    final int ALBUM_REQUEST_CODE = 0x0002;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSelectAlbum = findViewById(R.id.btn_select_album);
+        btnSelectPicture = findViewById(R.id.btn_pictures);
+        btnSelectAlbum.setOnClickListener(v -> {
+            Intent intent=new Intent();
+//            intent.setAction()
+        });
+
+    }
+
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityReenter(resultCode, data);
+        switch (resultCode) {
+//            case :
+        }
     }
 }
